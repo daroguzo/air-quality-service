f05ef576516c2d60f94ee4c4f5eb5caabe110d8e
daroguzo <kjw2122@naver.com>
2021-11-16 14:35:47 +0900

    25개 자치구 조회 쿼리로 변경

--- src/main/java/com/airqualityservice/seoul/SeoulAirQualityApi.java
+++ src/main/java/com/airqualityservice/seoul/SeoulAirQualityApi.java
@@ -6,6 +6,6 @@ import retrofit2.http.GET;
 public interface SeoulAirQualityApi {
     String serviceKey = "536c58687267757531334b46554b41";
 
-    @GET(serviceKey + "/json/RealtimeCityAir/1/5/")
+    @GET(serviceKey + "/json/RealtimeCityAir/1/25/")
     Call<SeoulAirQualityApiDto.GetAirQualityResponse> getAirQuality();
 }
