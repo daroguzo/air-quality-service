40f9e4b6e9c0552c65123a285f47d4f123492e0c
daroguzo <kjw2122@naver.com>
2021-11-22 14:47:56 +0900

    controller sidoCode와 gu를 따로 받을 수 있도록 수정

--- src/main/java/com/airqualityservice/application/controller/AirQualityController.java
+++ src/main/java/com/airqualityservice/application/controller/AirQualityController.java
@@ -1,11 +1,10 @@
 package com.airqualityservice.application.controller;
 
+import com.airqualityservice.application.Sido;
 import com.airqualityservice.application.service.SeoulService;
 import com.airqualityservice.dto.AirQualityDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/api/air_quality", produces = "application/json;charset=utf-8")
@@ -14,8 +13,9 @@ public class AirQualityController {
 
     private final SeoulService seoulService;
 
-    @GetMapping("/seoul")
-    public AirQualityDto seoul() {
-        return seoulService.seoulMapper();
+    @GetMapping("/{sidoCode}}")
+    public AirQualityDto getAirQuality(@PathVariable("sidoCode") Sido sidoCode,
+                                       @RequestParam(required = false) String gu) {
+        return seoulService.getAirQualityInfo(sidoCode, gu);
     }
 }
