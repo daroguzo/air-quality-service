c158053be47139f8b38edd343504207e1e65d449
daroguzo <kjw2122@naver.com>
2021-11-30 11:21:53 +0900

    cache 설정 추가

---
+++ src/main/java/com/airqualityservice/application/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.airqualityservice.application.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
